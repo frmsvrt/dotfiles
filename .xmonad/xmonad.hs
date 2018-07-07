6a9b9c3bfb01313777126dd8d43017f96dc27cea
Kravchanko Fyodor <udalite.feda@hotmail.com>
2018-07-07 19:11:09 +0800

    add audio buttons

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,6 +5,8 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Util.CustomKeys
 import XMonad.Util.EZConfig
 
+import qualified Graphics.X11.ExtraTypes.XF86 as XF86
+
 main = do
 xmproc <- spawnPipe "xmobar"
 xmonad $ defaultConfig
@@ -14,5 +16,11 @@ xmonad $ defaultConfig
   , logHook = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn xmproc, ppOrder = \(ws:_:t:_) -> [ws,t] }
   , borderWidth = 3
   }
-  `additionalKeys`
-  [ ((mod1Mask, xK_p), spawn "exe=`dmenu_path | /home/antares/.cabal/bin/yeganesh -- -b -fn xft:PragmataPro:size=12` && eval \"exec $exe\"") ]
+  `additionalKeys` [ ((mod1Mask, xK_p), spawn "exe=`dmenu_path | /home/sur/.cabal/bin/yeganesh -- -b -fn xft:PragmataPro:size=12` && eval \"exec $exe\"")
+                   , ((0, XF86.xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10")
+                   , ((0, XF86.xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
+                   , ((0, XF86.xF86XK_AudioRaiseVolume), spawn "amixer -D pulse sset Master 5%+")
+                   , ((0, XF86.xF86XK_AudioLowerVolume), spawn "amixer -D pulse sset Master 5%-")
+                   , ((0, XF86.xF86XK_AudioMute), spawn "amixer -D pulse sset Master mute")
+                   
+                   ]
